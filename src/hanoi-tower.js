4a144a3b19d9262f78e3fb859feb70bb2f1496b0
lyolya <Lelkabygr@mail.ru>
2021-02-22 09:57:30 +0400

    feat: add solution hanoi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,5 +1,5 @@
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
   const turns = Math.pow(2, disksNumber) - 1;
   const seconds = Math.floor((3600 / turnsSpeed) * turns);
-  return {turns, seconds}
+  return { turns, seconds };
 };
