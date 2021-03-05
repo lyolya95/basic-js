ca057bba662352a89c333b70be62f5989d0fdd99
lyolya <Lelkabygr@mail.ru>
2021-03-05 12:06:08 +0400

    feat: added solution vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,43 @@
-const CustomError = require("../extensions/custom-error");
-
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor (value) {
+    this.value = value;
+    this.alphabetUpperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    this.encryptText = '';
+  }
+
+  keyChange(message, key) {
+    if (!message || !key) throw new Error('Not parameters');
+    return key.repeat(Math.ceil(message.length / key.length));
+  }
+
+  result() {
+    const result = this.value === false ? this.encryptText.split('').reverse().join('') : this.encryptText;
+    this.encryptText = '';
+    return result;
+  }
+
+  encrypt(message, key) {
+    key = this.keyChange(message, key);
+    for (let i = 0; i < message.length; i++) {
+      let valueMessage = this.alphabetUpperCase.indexOf(message.toUpperCase()[i]);
+      if (valueMessage === -1) key = '1'+ key;
+      let valueKey = this.alphabetUpperCase.indexOf(key.toUpperCase()[i]);
+      this.encryptText += valueMessage > -1 ? this.alphabetUpperCase[(valueMessage + valueKey) % 26] : message[i];
+    }
+
+    return this.result();
+  }
+
+  decrypt(message, key) {
+    key = this.keyChange(message, key);
+    for (let i = 0; i < message.length; i++) {
+      let valueMessage = this.alphabetUpperCase.indexOf(message.toUpperCase()[i]);
+      if (valueMessage === -1) key = '1'+ key;
+      let valueKey = this.alphabetUpperCase.indexOf(key.toUpperCase()[i]);
+      this.encryptText += valueMessage > -1 ? this.alphabetUpperCase[(valueMessage - valueKey + 26) % 26] : message[i];
+    }
+
+    return this.result();
   }
 }
 
