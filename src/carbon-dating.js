04984088ccbfebe9f316541d8164ec406c667562
lyolya <Lelkabygr@mail.ru>
2021-03-05 13:02:28 +0400

    feat: edit carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,7 +2,7 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(num) {
-  if (typeof num !== 'string' || !num || isNaN(+num) || +num == 0) return false
+  if (typeof num !== 'string' || !num || isNaN(+num) || +num === 0) return false
   const result = Math.ceil(Math.log(MODERN_ACTIVITY / +num) / (0.693 / HALF_LIFE_PERIOD));
   return result > 0 && result;
 };
